9244d3dc4fb19dcdd854d29c9be53d6eb7040342
Nicolas Claverie <info@artscore-studio.fr>
2016-02-16 11:10:32 +0100

    Init bundle

---
+++ ASFWebsiteBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * (c) 2012-2015 Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ASF\WebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Website Bundle
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class ASFWebsiteBundle extends Bundle {}
---
+++ DependencyInjection/ASFWebsiteExtension.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of Artscore Studio Framework Package
+ *
+ * (c) 2012-2015 Artscore Studio <info@artscore-studio.fr>
+ *
+ * This source file is subject to the MIT Licence that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace ASF\WebsiteBundle\DependencyInjection;
+
+use ASF\CoreBundle\DependencyInjection\ASFExtension;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class ASFWebsiteExtension extends ASFExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+	    $config = $this->processConfiguration($configuration, $configs);
+	    
+	    $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    }
+}
